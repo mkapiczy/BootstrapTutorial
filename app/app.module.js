c0c2ea8560529106a4a3befb043dc5c0f4962052
mkapiczy <mix.kap@gmail.com>
2016-06-06 22:46:02 +0200

    [app.module] - routing

--- app/app.module.js
+++ app/app.module.js
@@ -1,4 +1,4 @@
-var crudApp = angular.module('crudApp', ['ngRoute']);
+var crudApp = angular.module('crudApp', ['ngRoute', 'ngMockE2E']);
 
 crudApp.config(['$routeProvider', function ($routeProvider) {
     $routeProvider
@@ -7,8 +7,21 @@ crudApp.config(['$routeProvider', function ($routeProvider) {
             templateUrl: 'app/components/home/homeView.html'
 
         })
-        .when('', {
-
+        .when('/employees', {
+            controller: 'EmployeeController',
+            templateUrl: 'app/components/employee/employeeView.html'
+        })
+        .when('/departments', {
+            controller: 'DepartmentController',
+            templateUrl: 'app/components/department/departmentView.html'
+        })
+        .when('/projects', {
+            controller: 'ProjectController',
+            templateUrl: 'app/components/project/projectView.html'
+        })
+        .when('/contact', {
+            controller: 'ContactController',
+            templateUrl: 'app/components/contact/contactView.html'
         })
         .otherwise({
             redirectTo: '/home'
