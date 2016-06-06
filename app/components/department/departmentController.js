ae7cbb602af78f15969006b17e73cd4fead2453e
mkapiczy <mix.kap@gmail.com>
2016-06-06 22:45:26 +0200

    [department] - wstępna struktura

---
+++ app/components/department/departmentController.js
@@ -0,0 +1,11 @@
+angular.module('crudApp')
+
+.controller('DepartmentController', ['$scope', '$log', function ($scope, $log ) {
+    var self = this;
+    $scope.departments = [];
+
+
+
+
+    }]);
+
